4411dd38f29ccb4ab698f111cdad5035080e6946
vagrant <vagrant@localhost.localdomain>
2020-09-07 01:25:49 +0000

    コメント削除機能

--- app/controllers/users/post_comments_controller.rb
+++ app/controllers/users/post_comments_controller.rb
@@ -9,6 +9,10 @@ class Users::PostCommentsController < ApplicationController
   end
 
   def destroy
+  	post = Post.find(params[:post_id])
+  	post_comment = post.post_comments.find(params[:id])
+  	post_comment.destroy
+  	redirect_to request.referer
   end
 
   private
