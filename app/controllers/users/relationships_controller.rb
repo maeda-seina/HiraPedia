8b11d2823175acc20671c106f4446c62f662ac19
vagrant <vagrant@localhost.localdomain>
2020-09-09 11:33:01 +0000

    コメント非同期

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,13 +1,19 @@
 class Users::RelationshipsController < ApplicationController
+  before_action :set_post
   def create
   	follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
   	follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
